afe999f2c730a0ee2a3b31699156736895b8317c
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2023-05-27 01:51:43 +0530

    after add contribution page and change styles in sidebar by aarju

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,43 +5,31 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className=" MyNavbar navbar navbar-expand-lg">
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarTogglerDemo03"
-        aria-controls="navbarTogglerDemo03"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <Link to={"/"} className="navbar-brand">
-        OSCF
-      </Link>
+      <div className="navbar-brand ml-lg-5">
+        <Link to={"/"} className="navbar-brand p-0">
+          <h4 className="p-0">OSCF</h4>
+        </Link>
+      </div>
       <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <form className="form-inline ml-auto mt-2 mt-lg-0">
+        <div className="form-inline ml-auto mt-2 mt-lg-0">
           <input
-            className="form-control mr-sm-2"
+            className="form-control mr-sm-2 "
             type="search"
             placeholder="Search"
             aria-label="Search"
+            size={30}
           />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
+          <button className="btn btn-primary   my-2 my-sm-0" type="button">
             Search
           </button>
-        </form>
+        </div>
         <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-          <li className="nav-item active">
+          <li className="nav-item mx-2">
             <Link to={"#"} className="nav-link">
               Contact Us
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item mx-2">
             <Link to={"/login"} className="nav-link">
               Login
             </Link>
