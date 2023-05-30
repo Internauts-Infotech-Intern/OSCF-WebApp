be4c64d8f33291209eea489721fc43fd1cc0de80
Meet Lad <meet48652@gmail.com>
2023-05-30 15:49:00 +0530

    Authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,38 +1,102 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const passport = require("passport");
+const dotenv = require("dotenv");
+dotenv.config();
 const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const cors = require("cors");
+// const bcrypt = require("bcrypt");
+router.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+
+  })
+);
 //REGISTER
 router.post("/signup", async (req, res) => {
-    try {
-     
-      const newUser = new User({
-       
-        email: req.body.email,
-        password: req.body.password
-      });
+  try {
+    if(req.body.password===req.body.cpassword){
+    const newUser = new User({
+      username:req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      phone:req.body.phone
+    });
   
-      const user = await newUser.save();
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(500).json(err);
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  }
+  else{
+    alert("Password and confirm password are not same");
+  }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  console.log("Login");
+  try {
+
+    const user = await User.findOne({ email: req.body.email });
+    console.log("user",user)
+    if (!user) {
+      res.send({ status: 0 })
+      console.log("User is not found")
+      return;
     }
-  });
-  
-  //LOGIN
-  router.post("/login", async (req, res) => {
-    try {
-  
-      const user = await User.findOne({ username: req.body.username });
-      if(!user) res.status(400).json(err);
-  
-      const validated = await bcrypt.compare(req.body.password, user.password);
-      if(!validated) res.status(400).json(err);
-  
-      const { password, ...others } = user._doc;
-      res.status(200).json(others);
-    } catch (err) {
-      res.status(500).json(err);
+    // const validated = await compare(req.body.password, user.password);
+    if (req.body.password!=user.password) {
+      res.send({ status: 2 })
+      console.log("Password is wrong")
+      return
     }
+
+    console.log("reach at status 1")
+    res.send({ status: 1, user: user });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
+
+
+router.get("/login/success", (req, res) => {
+  console.log("Login  Success");
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: "Successfully Loged In",
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({ error: true, message: "Not Authorized" });
+  }
+});
+
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: "Log in failure",
   });
-  
-  module.exports = router;
+});
+
+router.get("/google", passport.authenticate("google", ["profile", "email"]));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(process.env.CLIENT_URL);
+});
+
+module.exports = router;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,61 +1,153 @@
-import axios from "axios";
-import {  useRef } from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+// import axios from "axios";
+// import {  useRef } from "react";
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
 
-export default function Login() {
-    const userRef = useRef();
-    const passwordRef = useRef();
+// export default function Login() {
+//     const userRef = useRef();
+//     const passwordRef = useRef();
   
-    const [error, setError] = useState(false);
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+//     const [error, setError] = useState(false);
+//     const handleSubmit = async (e) => {
+//       e.preventDefault();
       
-      try {
-        const res = await axios.post("/auth/login", {
-          email: userRef.current.value,
-          password: passwordRef.current.value,
-        });
-        window.location.replace("/");
-      } catch (err) {
-        setError(true);
-      }
-    };
+//       try {
+//         const res = await axios.post("/auth/login", {
+//           email: userRef.current.value,
+//           password: passwordRef.current.value,
+//         });
+//         window.location.replace("/");
+//       } catch (err) {
+//         setError(true);
+//       }
+//     };
   
-    return (
-      <div>
-        <div>
-          <div>
-            <span><center>Login</center></span>
-            <form onSubmit={handleSubmit}>
-              <label>Email</label>
-              <input
-                type="text"
-                placeholder="Enter your email..."
-                ref={userRef}
-              />
-              <label>Password</label>
-              <input
-                type="password"
-                placeholder="Enter your password..."
-                ref={passwordRef}
-              />
-              <button type="submit" >
-                Login
-              </button>
+//     return (
+//       <div>
+//         <div>
+//           <div>
+//             <span><center>Login</center></span>
+//             <form onSubmit={handleSubmit}>
+//               <label>Email</label>
+//               <input
+//                 type="text"
+//                 placeholder="Enter your email..."
+//                 ref={userRef}
+//               />
+//               <label>Password</label>
+//               <input
+//                 type="password"
+//                 placeholder="Enter your password..."
+//                 ref={passwordRef}
+//               />
+//               <button type="submit" >
+//                 Login
+//               </button>
               
-            </form>
-            <div>If you don't have an account?</div>
-            <button>
-              <Link to="/signup">
-                Signup
-              </Link>
-            </button>
-            {error && <span style={{color:"red", marginTop:"10px"}}>Email or Password is wrong</span>}
-          </div>
-        </div>
+//             </form>
+//             <div>If you don't have an account?</div>
+//             <button>
+//               <Link to="/signup">
+//                 Signup
+//               </Link>
+//             </button>
+//             {error && <span style={{color:"red", marginTop:"10px"}}>Email or Password is wrong</span>}
+//           </div>
+//         </div>
   
-      </div>
-    );
-  }
+//       </div>
+//     );
+//   }
   
+import React, { Component, useContext } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useRef } from "react";
+import UserContext from '../../context/createcontext';
+function Login() {
+	const googleAuth = () => {
+		window.open(
+			`${process.env.REACT_APP_API_URL}/auth/google/callback`,
+			"_self"
+		);
+
+	};
+
+	const { user, setUser } = useContext(UserContext);
+
+	const userRef = useRef();
+	const passwordRef = useRef();
+	const navigate=useNavigate();
+	const [error, setError] = useState(false);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			console.log("Login");
+			console.log(userRef.current.value);
+			console.log(passwordRef.current.value);
+			axios.post("http://localhost:8080/api/auth/login", {
+				email: userRef.current.value,
+				password: passwordRef.current.value,
+			}).then((res)=>{
+				console.log(res);
+				
+				if(res.data.status=="1"){
+					setUser(res.data.user);
+					alert("Login Successful");
+
+					navigate("/");
+					
+				}
+			});
+			
+		} catch (err) {
+			setError(true);
+		}
+	};
+	return (
+		<div>
+			<h1>Log in Form</h1>
+			<div>
+				<div>
+					<img src="./images/login.jpg" alt="login" />
+				</div>
+				<div>
+					<h2>Members Log in</h2>
+					{/* <input type="text" className={styles.input} placeholder="Email" />
+					<input type="text" className={styles.input} placeholder="Password" />
+					<button className={styles.btn}>Log In</button> */}
+					<form onSubmit={handleSubmit}>
+						<label>Email</label>
+						<input
+							type="email"
+							placeholder="Enter your email..."
+							ref={userRef}
+						/>
+						<label>Password</label>
+						<input
+							type="password"
+							placeholder="Enter your password..."
+							ref={passwordRef}
+						/>
+						<button type="submit" >
+							Login
+						</button>
+
+					</form>
+					<p>or</p>
+					<button onClick={googleAuth}>
+						<img src="./images/google.png" alt="google icon" />
+						<span>Sing in with Google</span>
+					</button>
+					<p>
+						New Here ? <Link to="/signup">Sing Up</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
